e85be39df180636179f0bd4bdaf4571bcb1aa199
rupendrag <123197498+rupendrag@users.noreply.github.com>
2024-01-19 20:34:12 +0530

    Add files via upload

---
+++ Assignment 5 - Challenge3.py
@@ -0,0 +1,32 @@
+class Student:
+    def _init_(self):
+        self.__name = None
+        self.__roll_number = None
+    def getName(self):
+        return self.__name
+    def getRollNumber(self):
+        return self.__roll_number
+    def setName(self, name):
+        if isinstance(name, str):
+            self.__name = name
+        else:
+            raise ValueError("Invalid input for name. Name must be a string.")
+        
+    def setRollNumber(self, roll_number):
+        if isinstance(roll_number, str):
+            self.__roll_number = roll_number
+        else:
+            raise ValueError("Invalid input for roll number. Roll number must be a string.")
+
+student = Student()
+
+try:
+    name = input("Enter Students name ")
+    student.setName(name)
+    roll_number = input("Enter Roll number")
+    student.setRollNumber(roll_number)
+
+    print(f"Name: {student.getName()}")
+    print(f"Roll Number: {student.getRollNumber()}")
+except ValueError as e:
+    print(e)
